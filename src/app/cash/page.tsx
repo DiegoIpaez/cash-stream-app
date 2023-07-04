ac39986a409ffa7112d3182d810c0945d64575fe
Diegoipaez <diegokpo1313@gmail.com>
2023-07-04 20:34:59 -0300

    added pages

---
+++ src/app/cash/page.tsx
@@ -0,0 +1,7 @@
+export default function Cash() {
+  return (
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1>Cash</h1>
+    </div>
+  );
+}
---
+++ src/app/history/page.tsx
@@ -0,0 +1,7 @@
+export default function History() {
+  return (
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1>History</h1>
+    </div>
+  );
+}
