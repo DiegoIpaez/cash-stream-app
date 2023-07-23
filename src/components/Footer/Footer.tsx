548bf5cba6c714a7b5f051889d8a3f0f31517ef2
Diegoipaez <diegokpo1313@gmail.com>
2023-07-22 21:12:17 -0300

    aded github link in to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { GithubOutline } from "../UI/Icons";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-600 py-5 text-center w-full mt-8">
-      <p className="text-white">
-        © 2023 Cash stream. Todos los derechos reservados.
-      </p>
+      <div className="flex flex-col md:flex-row justify-around items-center">
+        <a
+          className="flex"
+          target="_blank"
+          href="https://github.com/DiegoIpaez?tab=repositories"
+          rel="noopener noreferrer"
+        >
+          <GithubOutline width={17} />
+          <p className="ms-2">DiegoIPaez/repositories</p>
+        </a>
+        <p className="text-white mb-2 md:mb-0">
+          © 2023 Cash stream.
+        </p>
+      </div>
     </footer>
   );
 }
--- src/components/UI/Icons/index.ts
+++ src/components/UI/Icons/index.ts
@@ -1,2 +1,3 @@
-export { default as DeleteOutline } from './DeleteOutline';
-export { default as LoadingOutline } from './LoadingOutline';
+export { default as DeleteOutline } from "./DeleteOutline";
+export { default as LoadingOutline } from "./LoadingOutline";
+export { default as GithubOutline } from "./GithubOutline";
